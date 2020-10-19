d2530aed2b195fbdcc8ef4f57fbef63fde43c76b
Problocker <55052514+Problocker@users.noreply.github.com>
2020-10-19 11:19:47 +0200

    Add project files.

---
+++ DRRecordREST/Controllers/RecordsController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ModelLIb.model;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DRRecordREST.Controllers
+{
+   private static  readonly List<>
+
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecordsController : ControllerBase
+    {
+        // GET: api/<RecordsController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<RecordsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<RecordsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<RecordsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<RecordsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ModelLIb/model/Records.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelLIb.model
+{
+    public class Records
+    {
+        private string _title;
+        private string _artist;
+        private int _duration;
+        private string _yearOfPublication;
+        private string _countryorigin;
+
+        public Records()
+        {
+        }
+
+        public Records(string title, string artist, int duration, string yearOfPublication, string countryorigin)
+        {
+            _title = title;
+            _artist = artist;
+            _duration = duration;
+            _yearOfPublication = yearOfPublication;
+            _countryorigin = countryorigin;
+        }
+
+
+        public string Title
+        {
+            get => _title;
+            set => _title = value;
+        }
+
+        public string Artist
+        {
+            get => _artist;
+            set => _artist = value;
+        }
+
+        public int Duration
+        {
+            get => _duration;
+            set => _duration = value;
+        }
+
+        public string YearOfPublication
+        {
+            get => _yearOfPublication;
+            set => _yearOfPublication = value;
+        }
+
+        public string Countryorigin
+        {
+            get => _countryorigin;
+            set => _countryorigin = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Title)}: {Title}, {nameof(Artist)}: {Artist}, {nameof(Duration)}: {Duration}, {nameof(YearOfPublication)}: {YearOfPublication}, {nameof(Countryorigin)}: {Countryorigin}";
+        }
+    }
+
+}
